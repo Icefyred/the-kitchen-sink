55e75e2f48a9b61988fa20aa4bf6eec3174e6e89
Icefyred <dcastilho001@hotmail.com>
2019-03-30 17:47:43 +0000

    Started a new Spring Repo

---
+++ src/test/java/pt/compta/thekitchensink/TheKitchenSinkApplicationTests.java
@@ -0,0 +1,16 @@
+package pt.compta.thekitchensink;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheKitchenSinkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
